1c7b0207890cb7f1e887baa58600440f8626f6d8
verixx <abdurraqeeb53@gmail.com>
2017-12-29 04:20:17 +1100

    Sqlite dictionary for easy usage

--- clashroyale/utils.py
+++ clashroyale/utils.py
@@ -95,3 +95,99 @@ class Endpoints:
     TOURNAMENT = BASE + '/tournaments'
     ENDPOINTS = BASE + '/endpoints'
     VERSION = BASE + '/version'
+
+from collections import MutableMapping
+import sqlite3 as sqlite
+from contextlib import contextmanager
+import threading
+import pickle
+
+
+class SqliteDict(MutableMapping):
+    def __init__(self, filename, table_name='data', fast_save=False, **options):
+        self.filename = filename
+        self.table_name = table_name
+        self.fast_save = fast_save
+        self.can_commit = True
+        self._bulk_commit = False
+        self._pending_connection = None
+        self._lock = threading.RLock()
+        with self.connection() as con:
+            con.execute("create table if not exists `%s` (key PRIMARY KEY, value)" % self.table_name)
+
+    @contextmanager
+    def connection(self, commit_on_success=False):
+        with self._lock:
+            if self._bulk_commit:
+                if self._pending_connection is None:
+                    self._pending_connection = sqlite.connect(self.filename)
+                con = self._pending_connection
+            else:
+                con = sqlite.connect(self.filename)
+            try:
+                if self.fast_save:
+                    con.execute("PRAGMA synchronous = 0;")
+                yield con
+                if commit_on_success and self.can_commit:
+                    con.commit()
+            finally:
+                if not self._bulk_commit:
+                    con.close()
+
+    def commit(self, force=False):
+        if force or self.can_commit:
+            if self._pending_connection is not None:
+                self._pending_connection.commit()
+
+    @contextmanager
+    def bulk_commit(self):
+        self._bulk_commit = True
+        self.can_commit = False
+        try:
+            yield
+            self.commit(True)
+        finally:
+            self._bulk_commit = False
+            self.can_commit = True
+            if self._pending_connection is not None:
+                self._pending_connection.close()
+                self._pending_connection = None
+
+    def __getitem__(self, key):
+        with self.connection() as con:
+            row = con.execute("select value from `%s` where key=?" %
+                              self.table_name, (key,)).fetchone()
+            if not row:
+                raise KeyError
+            return pickle.loads(row[0])
+
+    def __setitem__(self, key, item):
+        with self.connection(True) as con:
+            con.execute("insert or replace into `%s` (key,value) values (?,?)" %
+                        self.table_name, (key, pickle.dumps(item)))
+
+    def __delitem__(self, key):
+        with self.connection(True) as con:
+            cur = con.execute("delete from `%s` where key=?" %
+                              self.table_name, (key,))
+            if not cur.rowcount:
+                raise KeyError
+
+    def __iter__(self):
+        with self.connection() as con:
+            for row in con.execute("select key from `%s`" % self.table_name):
+                yield row[0]
+
+    def __len__(self):
+        with self.connection() as con:
+            return con.execute("select count(key) from `%s`" %
+                               self.table_name).fetchone()[0]
+
+    def clear(self):
+        with self.connection(True) as con:
+            con.execute("drop table `%s`" % self.table_name)
+            con.execute("create table `%s` (key PRIMARY KEY, value)" %
+                        self.table_name)
+
+    def __str__(self):
+        return str(dict(self.items()))
