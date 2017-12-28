466057c94175ab0014365e9ba1a3c4b164519b5a
verixx <abdurraqeeb53@gmail.com>
2017-12-28 16:49:19 +1100

    Update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='clashroyale',
     packages=['clashroyale'],  
-    version='v3.0.2',
+    version='v3.0.3',
     description='An async wrapper for cr-api.com',
     author='kyb3r',
     license='MIT',
