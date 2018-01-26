7cb8542ff5fdc86b42ad5339a90707275505a898
verixx <abdurraqeeb53@gmail.com>
2018-01-27 04:38:13 +1100

    Increment version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='clashroyale',
     packages=['clashroyale'],  
-    version='v3.1.11',
+    version='v3.1.12',
     description='An async wrapper for cr-api.com',
     author='kyb3r',
     license='MIT',
