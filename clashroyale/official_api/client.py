0ec6ee202237ba6404e21a787e306ee1821fa286
fourjr <28086837+fourjr@users.noreply.github.com>
2018-08-11 19:17:25 +0800

    3.5.6 - adds a get_datetime function (util func)

--- clashroyale/official_api/client.py
+++ clashroyale/official_api/client.py
@@ -424,3 +424,24 @@ class Client:
             deck_link += f'{card.id};'
 
         return deck_link
+
+    def get_datetime(self, timestamp: str, unix=True):
+        """Converts a %Y%m%dT%H%M%S.%fZ to a UNIX timestamp
+        or a datetime.datetime object
+
+        Parameters
+        ---------
+        timestamp: str
+            A timstamp in the %Y%m%dT%H%M%S.%fZ format, usually returned by the API
+            in the ``created_time`` field for example (eg. 20180718T145906.000Z)
+        unix: Optional[bool]
+            Whether to return a POSIX timestamp (seconds since epoch) or not
+            (default: True)
+
+        Returns int or datetime.datetime
+        """
+        time = datetime.strptime(timestamp, '%Y%m%dT%H%M%S.%fZ')
+        if unix:
+            return time.timestamp()
+        else:
+            return time
