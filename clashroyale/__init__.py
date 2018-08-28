adeb8fb704b5a59b19a1e8bcf3a548d111e7624c
fourjr <28086837+fourjr@users.noreply.github.com>
2018-08-28 17:12:40 +0800

    Add PyPi

--- clashroyale/__init__.py
+++ clashroyale/__init__.py
@@ -30,7 +30,7 @@ from .errors import *
 # METADATA #
 ############
 
-__version__ = 'v3.5.14'
+__version__ = 'v3.5.15'
 __title__ = 'clashroyale'
 __license__ = 'MIT'
 __github__ = 'https://github.com/cgrok/clashroyale'
