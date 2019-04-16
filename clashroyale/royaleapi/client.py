e9dd14d746934da15f0bdaf89321365310772c58
gogaz <5682364+gogaz@users.noreply.github.com>
2019-04-16 18:55:03 +0200

    Add Top War Clans endpoint
    The "Top War Clans" endpoint from RoyaleAPI can now be reached natively

--- clashroyale/royaleapi/client.py
+++ clashroyale/royaleapi/client.py
@@ -622,6 +622,31 @@ class Client:
         """
         url = self.api.TOP + '/clans/' + str(country_key)
         return self._get_model(url, PartialClan, **params)
+      
+    @typecasted
+    def get_top_war_clans(self, country_key='', **params: keys):
+        """Get a list of top clans by war
+
+        location_id: Optional[str] = ''
+            A location ID or '' (global)
+            See https://github.com/RoyaleAPI/cr-api-data/blob/master/json/regions.json
+            for a list of acceptable location IDs
+        \*\*keys: Optional[list] = None
+            Filter which keys should be included in the
+            response
+        \*\*exclude: Optional[list] = None
+            Filter which keys should be excluded from the
+            response
+        \*\*max: Optional[int] = None
+            Limit the number of items returned in the response
+        \*\*page: Optional[int] = None
+            Works with max, the zero-based page of the
+            items
+        \*\*timeout: Optional[int] = None
+            Custom timeout that overwrites Client.timeout
+        """
+        url = self.api.TOP + '/war/' + str(country_key)
+        return self._get_model(url, PartialClan, **params)
 
     @typecasted
     def get_top_players(self, country_key='', **params: keys):
